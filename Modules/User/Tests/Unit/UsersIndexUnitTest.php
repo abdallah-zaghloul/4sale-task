fc59cbc7b413699f3894ec2bb77262f2f5833183
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2023-11-09 10:49:21 +0200

    Build User Module

---
+++ Modules/User/Tests/Unit/UsersIndexUnitTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\User\Tests\Unit;
+
+use Illuminate\Support\Benchmark;
+use Modules\User\Database\Seeders\UserDatabaseSeeder;
+use Modules\User\Services\UsersIndexService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UsersIndexUnitTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_users_index_service()
+    {
+        app(UserDatabaseSeeder::class)->run(2,1000);
+        $result = fn() => app(UsersIndexService::class)->execute();
+        $this->assertNotEmpty($result());
+        $benchmark = fn(): array|float => Benchmark::measure(fn() => $result());
+        echo(
+            str_repeat(' ',5)
+            .'Query BenchMark Result = '
+            .(is_array($benchmark()) ? implode(PHP_EOL, $benchmark()) : $benchmark())
+        );
+    }
+}
